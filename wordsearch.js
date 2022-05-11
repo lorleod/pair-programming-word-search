1469e61d24f409518e00b5c1855b27fe063609a6
lorleod <taylor.mcleod@gmail.com>
2022-05-11 23:05:42 +0000

    MADE THE THING

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = require(`./transpose`);
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+	const horizontalJoin = letters.map((ls) => ls.join(""));
+
+	for (l of horizontalJoin) {
+		if (l.includes(word)) return true;
+	}
+
+	const transposed = transpose(letters);
+
+	const verticalJoin = transposed.map((ls) => ls.join(""));
+
+	for (l of verticalJoin) {
+		if (l.includes(word)) return true;
+	}
+
+	return false;
+};
+
+module.exports = wordSearch;
+
